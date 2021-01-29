a8e111e60d146f2c0817d6c5f7c1cdc201c57489
jingdd-boop <1736929925@qq.com>
2021-01-29 20:52:03 +0800

    二叉树中序遍历

---
+++ 数据结构与算法/二叉树/二叉树Leetcode/94.二叉树的中序遍历/1.js
@@ -0,0 +1,31 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  const res = [];
+  const stack = [];
+  let p = root;
+ while(stack.length || p) {
+      while(p) {
+      stack.push(p);
+      p = p.left;
+  }
+  const n = stack.pop();
+  res.push(n.val);
+  p = n.right
+ }
+ return res
+
+
+//     const res = []
+//     const rec = (n) => {
+//         if(!n) {return}
+//         rec(n.left);
+//         res.push(n.val)
+//         rec(n.right)
+//     }
+//     rec(root);
+ 
+//    return res
+};
